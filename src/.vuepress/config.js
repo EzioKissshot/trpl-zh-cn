7fab95901b27c52e2409b2d05756e6598ad0c6dc
Ezio Shiki <zzyagg@gmail.com>
2019-10-20 18:44:52 +0800

    侧边栏调不出来啊...

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,3 +1,10 @@
 module.exports = {
-    dest: 'public'
+    dest: 'public',
+    title: 'Rust 程序设计语言',
+    themeConfig: {
+        displayAllHeaders: true,
+        search: false,
+        searchMaxSuggestions: 10,
+        smoothScroll: true
+    }
 }
